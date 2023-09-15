5209d0437f8fbdb524ff42a70b5686b492165739
dongi <dongibson633@gmail.com>
2023-09-15 14:11:18 -0400

    Commit 1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,17 @@ public class Main
 {
     public static void main(String[] args)
     {
-        
+        double initial = 5000;
+        double monthOne = 0;
+        double monthTwo = 0;
+        double interest = 1.17;
+
+        monthOne = initial * interest;
+        monthTwo = monthOne * interest;
+
+        System.out.println("Your initial balance is " + initial);
+        System.out.println("Your balance after one month is " + monthOne);
+        System.out.println("Your balance after two months is " + monthTwo);
 
     }
 }
